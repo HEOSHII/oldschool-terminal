744cfdc6c06ecf6c23830a02b3bc8f89c4f13ddd
George Shiryaev <george.s@mencodehard.com>
2023-04-04 12:09:32 +0300

    added mobile browser support

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,18 @@
 <script setup>
 import Time from './Time.vue'
 
+const { inChat } = defineProps(['inChat']);
+
 </script>
 
 <template>
   <header class="fixed top-0 w-full py-4 bg-[#000000b0] backdrop-blur-lg">
     <div class="flex justify-between items-center px-10">
-      <a href="/" class="text-4xl">Terminal<span class="animate-pulse">_</span></a>
+      <a href="/" class="text-3xl">Terminal<span class="animate-pulse">_</span></a>
+      <marquee class="text-xl" v-if="inChat" width="500" scrolldelay="1" behavior="scroll" direction="left">
+        You in chat with Open AI. You can ask anything you want and Open AI will answer to you. Enter
+        EXIT CHAT for leave
+      </marquee>
       <Time />
     </div>
   </header>
--- src/components/StartingPage.vue
+++ src/components/StartingPage.vue
@@ -18,9 +18,12 @@ onUnmounted(() => document.removeEventListener('keydown', enterListener));
   <div class=" bg-black text-white w-screen h-screen flex flex-col justify-center items-center">
     <p class="text-4xl text-red uppercase mb-14">Warning!</p>
     <p class="text-5xl mb-6">This web-app can play <span class="animate-text-stereo">sounds</span></p>
-    <p class="text-3xl max-w-lg text-center mb-14">By default sounds are turned OFF but you always can turn them ON from
-      the terminal</p>
-    <p class="text-2xl animate-pulse underline"> Press <span class="text-terminal-green-primary">ENTER</span>
-      to get started</p>
+    <p class="text-3xl max-w-lg text-center mb-14">
+      By default sounds are turned OFF but you always can turn it ON from the terminal
+    </p>
+    <p class="text-2xl animate-pulse underline"> Press
+      <span class="text-terminal-green-primary">ENTER</span>
+      to get started
+    </p>
   </div>
 </template>
