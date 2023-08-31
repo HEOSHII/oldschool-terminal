3dfe8b2d169fc56d5e59c1388b63ab25de703bd9
George Shiryaev <george.shiryaev@gmail.com>
2023-08-31 13:28:28 +0300

    remove properties

--- server/index.js
+++ server/index.js
@@ -21,9 +21,6 @@ app.post('/chat', async (req, res) => {
 	const content = req.body.question;
 	const { data } = await openai.createChatCompletion({
 		model: 'gpt-3.5-turbo',
-		temperature: 0.9,
-		maxTokens: 150,
-
 		messages: [{ role: 'user', content: content + '. Speak like terminal robot' }],
 	});
 	res.json(data.choices[0].message);
